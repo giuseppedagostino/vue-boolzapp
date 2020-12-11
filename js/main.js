aea74250db340e75f3d1e0b6fa05a17b2051425b
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-12-11 22:33:08 +0100

    Milestone 1 completata

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-// MILESTONE 1
-// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
-// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+// MILESTONE 2
+// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// Click sul contatto mostra la conversazione del contatto cliccato
 
 var app = new Vue({
   el: "#root",
@@ -33,7 +33,7 @@ var app = new Vue({
 
       // secondo avatar
       {
-        name: "Fabio",
+        name: "Chiara",
         avatar: "_2",
         visible: true,
         messages: [
@@ -57,7 +57,7 @@ var app = new Vue({
 
       // terzo avatar
       {
-        name: "Michele",
+        name: "Michela",
         avatar: "_3",
         visible: true,
         messages: [
@@ -101,6 +101,8 @@ var app = new Vue({
     ]
   },
   methods: {
-
+    getAvatar: function(contact) {
+      return `img/${contact.avatar}.png`;
+    }
   }
 });
